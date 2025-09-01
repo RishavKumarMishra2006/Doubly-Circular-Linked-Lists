bb673c4e01b37003ea2edf840446ff116df38405
Rishav_Kumar_Mishra <rishavkumarmishra001@gmail.com>
2025-09-01 12:31:21 +0530

    Create Check if linked list is circular.c

---
+++ Check if linked list is circular.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+struct Node {
+    int data;
+    struct Node* next;
+    struct Node* prev;   // for DLL
+};
+
+// For multi-level linked list flattening
+struct NodeChild {
+    int data;
+    struct NodeChild* next;
+    struct NodeChild* bottom;
+};
+
+struct Node* newNode(int data) {
+    struct Node* node = (struct Node*)malloc(sizeof(struct Node));
+    node->data = data;
+    node->next = NULL;
+    node->prev = NULL;
+    return node;
+}
+bool isCircular(struct Node* head) {
+    if (!head) return true;
+    struct Node* temp = head->next;
+    while (temp && temp != head) temp = temp->next;
+    return temp == head;
+}
